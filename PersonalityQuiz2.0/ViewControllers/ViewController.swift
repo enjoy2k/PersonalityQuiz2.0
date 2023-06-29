51807eafa3c58dcca74e3dd0cde63455def1b394
Daniil Kozlov <Enjoy2k@mail.ru>
2023-06-29 17:23:22 +0300

    Initial Commit

---
+++ PersonalityQuiz2.0/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz2.0
+//
+//  Created by Даниил Козлов on 22.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
